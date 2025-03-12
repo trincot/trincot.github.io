57c3d0726ec90e1c52ffbb6c4ca622956a4b43e2
trincot <tkoornstra@yahoo.com>
2025-03-12 21:02:24 +0100

    Add undo

--- turing.js
+++ turing.js
@@ -10,6 +10,7 @@ class Turing {
         this.initState = initState;
         this.markdown = Turing.markdown(transitions);
         this.blank = blank;
+        this.history = [];
     }    
     load(tape, shift=1, size=2) {
         tape = tape || this.blank,
@@ -18,6 +19,7 @@ class Turing {
         this.index = shift;
         this.count = 0;
         this.unshiftCount = shift;
+        this.history.length = 0;
     }
     step() {
         const transition = this.transition();
@@ -25,6 +27,7 @@ class Turing {
         transition.count++;
         this.count++;
         const {write, nextState, move} = transition;
+        this.history.push({transition, undo: { write: this.tape[this.index], move: -move, nextState: this.state }});
         if (write) this.tape[this.index] = write;
         this.state = nextState;
         this.index += move;
@@ -34,6 +37,18 @@ class Turing {
             this.index++;
             this.unshiftCount++;
         }
+        return true;
+    }
+    undo() {
+        const popped = this.history.pop();
+        if (!popped) return false;
+        const {transition, undo} = popped;
+        transition.count--;
+        this.count--;
+        const {nextState, move, write} = undo;
+        this.state = nextState;
+        this.index += move;
+        this.tape[this.index] = write;
         return true;
     }
     run(stepLimit=1000) {
@@ -93,16 +108,16 @@ class Presentation {
     .blank { background: #eee }
     .highlight { background: yellow }
     .highlight.selected { background: gold }
-</style>
+<\/style>
 Input: <input><button>Load<\/button><br>
 Tape:
 <table><tr><\/tr><\/table>
 State: <span><\/span><br>
 Count: <span><\/span><br>
-<button>Step<\/button><button>Play<\/button>
+<button>Step<\/button><button>Play<\/button><button>Undo<\/button>
         `;
         [this.input, this.output, this.stateOut, this.counter] = document.querySelectorAll("input,tr,span");
-        [this.load, this.step, this.play] = document.querySelectorAll("button");
+        [this.load, this.step, this.play, this.undo] = document.querySelectorAll("button");
         this.timer = -1;
     }
     display(turing, logState) {
@@ -116,9 +131,10 @@ Count: <span><\/span><br>
     }
 }
 
-function createTuring({transitions, initState, blank, tape, tests, logState, stepLimit=10000}) {
+function createTuring({transitions, initState, blank, tape, tests, logState, stepLimit=100000}) {
     const turing = new Turing(transitions, initState, blank ?? "_");
     const view = new Presentation();
+    let delay = Infinity;
     view.load.onclick = () => {
         clearTimeout(view.timer);
         turing.load(view.input.value);
@@ -126,26 +142,35 @@ function createTuring({transitions, initState, blank, tape, tests, logState, ste
         turing.load(view.input.value, turing.unshiftCount, turing.tape.length);
         view.display(turing);
     };
-    view.step.onclick = () => {
+    function step(newDelay=Infinity) {
         clearTimeout(view.timer);
-        turing.step();
+        if (newDelay > 0 ? turing.halted() : !turing.history.length) {
+            delay = Infinity;
+            return false;
+        }
+        delay = newDelay;
+        // Perform steps in batches when the speed is high (the absolute delay is less than 1)
+        for (let tick = 0; tick < 1 && (delay < 0 || turing.state !== logState); tick += Math.abs(delay)) {
+            delay > 0 ? turing.step() : turing.undo(); // Negative delays indicate backwards "time traversal"
+        }
         view.display(turing, logState);
-    };
-    view.play.onclick = () => {
-        clearTimeout(view.timer);
-        view.timer = setTimeout(() => {
-            view.step.onclick();
-            view.play.onclick();
-        }, turing.state === logState ? 500 : 100);
-    };
+        return true;
+    }
+    function play(newDelay) {
+        if (!step(newDelay ?? delay) || !Number.isFinite(delay)) return;
+        view.timer = setTimeout(play, turing.state === logState ? 500 : Math.abs(delay));
+    }
+    view.step.onclick = () => play(Infinity);
+    view.play.onclick = () => play(!Number.isFinite(delay) ? 100 : delay / 100);
+    view.undo.onclick = () => play(-Infinity);
     view.input.value = tape;
     for (const [tape, expected] of tests ?? []) {
         turing.load(tape);
         turing.run(stepLimit);
+        console.log(tape.length, turing.count);
         console.assert([turing.state, turing.output()].includes(expected), `failed test: ${tape}. Expected ${expected}, got state=${turing.state}, output=${turing.output()}`);
     }
     view.load.onclick();
     return turing.markdown + "\n\nUnused transitions:\n" 
          + JSON.stringify(turing.unusedTransitions(), null, 2);
 }
-
